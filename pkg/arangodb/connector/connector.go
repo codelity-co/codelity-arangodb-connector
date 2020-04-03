e561de17844516df7cc8d7ab9834db48d760bef2
reidlai <reidlai@gmail.com>
2020-04-03 19:35:03 +0800

    normalize connection method

--- pkg/arangodb/connector/connector.go
+++ pkg/arangodb/connector/connector.go
@@ -8,7 +8,7 @@ import (
 	"github.com/arangodb/go-driver/http"
 )
 
-type ArangoDbConnector struct {
+type ArangodbConnector struct {
 	ArangoDbUrls string
 	CollectionName string
 	Collection godriver.Collection
@@ -24,76 +24,76 @@ var (
 	ctx context.Context = context.Background()
 )
 
-func(c *ArangoDbConnector) Connect() error {
+func(c *ArangodbConnector) Connect() error {
 	var err error
-
 	c.Connection, err = http.NewConnection(http.ConnectionConfig{
 		Endpoints: []string{c.ArangoDbUrls},
 		TLSConfig: &tls.Config{InsecureSkipVerify: true},
 	})
-	
-	if err != nil {
-		return err
-	}
+	return err
+}
 
+func(c *ArangodbConnector) NewClient() error {
+	var err error
 	clientConfig := godriver.ClientConfig{
 		Connection: c.Connection,
 		Authentication: godriver.BasicAuthentication(c.UserName, c.UserPassword),
 	}
 	c.Client, err = godriver.NewClient(clientConfig)
-	if err != nil {
-		return err
-	}
+	return err
+}
 
-	var dbExist bool
-	dbExist, err = c.Client.DatabaseExists(ctx, c.DatabaseName) 
-	if err != nil {
-		return err
-	}
-	if !dbExist {
-		createDbOptions := &godriver.CreateDatabaseOptions{}
-		c.Database, err = c.Client.CreateDatabase(ctx, c.DatabaseName, createDbOptions)
-		if err != nil {
-			return err
-		}
-	} else {
-		c.Database, err = c.Client.Database(ctx, c.DatabaseName)
-		if err != nil {
-			return err
-		}
-	}
-
-	var colExist bool
-	colExist, err = c.Database.CollectionExists(ctx, c.CollectionName)
-	if err != nil {
-		return err
-	}
-
-	if !colExist {
-		createColOptions := &godriver.CreateCollectionOptions{}
-		c.Collection, err = c.Database.CreateCollection(ctx, c.CollectionName, createColOptions)
-		if err != nil {
-			return err
-		}
-	} else {
-		c.Collection, err = c.Database.Collection(ctx, c.CollectionName)
-		if err != nil {
-			return err
-		}
-	}
-
+func(c *ArangodbConnector) Close () error {
 	return nil
 }
 
-func(c *ArangoDbConnector) CreateDocument(doc interface{}) (*godriver.DocumentMeta, error) {
+func(c *ArangodbConnector) DatabaseExists() (*bool, error) {
+	var (
+		dbExist bool
+		err error
+	)
+	dbExist, err = c.Client.DatabaseExists(ctx, c.DatabaseName) 
+	if err != nil {
+		return nil, err
+	}
+	return &dbExist, nil
+}
+
+func(c *ArangodbConnector) OpenDatabase() error {
+	var err error
+	c.Database, err = c.Client.Database(ctx, c.DatabaseName)
+	return err
+}
+
+func(c *ArangodbConnector) CollectionExists() (*bool, error) {
+	var (
+		colExist bool
+		err error
+	)
+	colExist, err = c.Database.CollectionExists(ctx, c.CollectionName)
+	if err != nil {
+		return nil, err
+	}
+	return &colExist, nil
+
+}
+
+func(c *ArangodbConnector) OpenCollection() error {
+	var err error
+	c.Collection, err = c.Database.Collection(ctx, c.CollectionName)
+	return err
+}
+
+func(c *ArangodbConnector) CreateDocument(doc interface{}) (*godriver.DocumentMeta, error) {
 	meta, err := c.Collection.CreateDocument(ctx, doc)
 	if err != nil {
 		return nil, err
 	}
 	return &meta, nil
+
 }
 
-func(c *ArangoDbConnector) UpdateDocument(key string, doc interface{}) (*godriver.DocumentMeta, error) {
+func(c *ArangodbConnector) UpdateDocument(key string, doc interface{}) (*godriver.DocumentMeta, error) {
 	meta, err := c.Collection.UpdateDocument(ctx, key, doc)
 	if err != nil {
 		return nil, err
@@ -101,7 +101,7 @@ func(c *ArangoDbConnector) UpdateDocument(key string, doc interface{}) (*godrive
 	return &meta, nil
 }
 
-func(c *ArangoDbConnector) DeleteDocument(key string) (*godriver.DocumentMeta, error) {
+func(c *ArangodbConnector) DeleteDocument(key string) (*godriver.DocumentMeta, error) {
 	meta, err := c.Collection.RemoveDocument(ctx, key)
 	if err != nil {
 		return nil, err
